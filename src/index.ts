9079b87d0d747d7819a66d19a9e4bcf4863fa53b
Rafal Pijet <stronglopez@wp.pl>
2021-01-28 13:42:55 +0100

    Add typical solution

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
 import loginRouter from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['string'] }))
 app.use(loginRouter);
 
 app.listen(3000, (): void => {
